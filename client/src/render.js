674d3611a7cb4a7597b127f49fdc61dd6e24360e
Florian Krauthan <mail@fkrauthan.de>
2014-09-27 19:43:10 -0700

    Added a flux implementation

--- client/src/render.js
+++ client/src/render.js
@@ -2,13 +2,19 @@
 var fs = require('fs');
 var url = require('url');
 
+var Fluxy = require('fluxy');
+
 module.exports = function(CONFIG) {
-    var PLACEHOLDER = '<!-- CONTENT -->';
+    var PLACEHOLDER_APP = '<!--app-->';
+    var PLACEHOLDER_BOOTSTRAP = '/*bootstrap*/';
     var TEMPLATE = fs.readFileSync(__dirname + '/../client/layout.html', {encoding: 'utf8'});
 
     return function(req, res, next) {
         var path = url.parse(req.url).pathname;
 
-        res.send(TEMPLATE.replace(PLACEHOLDER, path));
+        Fluxy.start({});
+
+
+        res.send(TEMPLATE.replace(PLACEHOLDER_APP, path).replace(PLACEHOLDER_BOOTSTRAP, 'window.__fluxy__ = ' + Fluxy.renderStateToString()));
     };
 };

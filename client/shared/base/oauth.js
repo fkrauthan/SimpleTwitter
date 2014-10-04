60553405076858072f5f5d6525fead4c1795d701
Florian Krauthan <mail@fkrauthan.de>
2014-10-03 21:51:32 -0700

    Optimized oauth signing

--- client/shared/base/oauth.js
+++ client/shared/base/oauth.js
@@ -1,21 +1,53 @@
 
 var OAuth = require('OAuth');
 
-module.exports = function(config, userConfig) {
+function isHost(obj) {
+    var str = {}.toString.call(obj);
+
+    switch (str) {
+        case '[object File]':
+        case '[object Blob]':
+        case '[object FormData]':
+            return true;
+        default:
+            return false;
+    }
+}
+
+module.exports = function(config) {
     return function(request) {
         var oauth = new OAuth('#',
             '#',
-            config.api.app_token,
-            config.api.app_secret,
+            config.app_token,
+            config.app_secret,
             '1.0A',
             null,
             'HMAC-SHA1');
 
+        var method = request.method;
+
+        var url = request.url;
+        var query = request._query.join('&');
+        if (query) {
+            query = request.serializeObject(query);
+            url += ~url.indexOf('?')
+                ? '&' + query
+                : '?' + query;
+        }
+
+        var data = request._formData || request._data;
+        if ('GET' != method && 'HEAD' != method && 'string' != typeof data && !isHost(data)) {
+            // serialize stuff
+            var serialize = request.serialize[request.getHeader('Content-Type')];
+            if (serialize) data = serialize(data);
+        }
+
         var orderedParams = oauth._prepareParameters(
-            userConfig.api.user_token,
-            userConfig.api.user_secret,
-            request.method,
-            request.url
+            config.user_token,
+            config.user_secret,
+            method,
+            url,
+            data
         );
 
         request.set('Authorization', oauth._buildAuthorizationHeaders(orderedParams));
